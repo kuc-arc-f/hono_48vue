de1240b25afa86ba30eddb13c934bf003ea4833c
_ñ <knaka0209@gmail.com>
2024-01-14 16:38:01 +0900

    up, 0114a6

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <router-link to="/">[ Home ]</router-link>
+    <router-link to="/about">[ About ]</router-link>
+</template>
+
+<style scoped>
+</style>
